8985d395af315148c24e82186212b1a63187c1b5
Sean OConnor <soconnor@yahoo.com>
2017-06-01 20:33:46 -0500

    Add project files.

---
+++ AwsParameterStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwsParameterStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
